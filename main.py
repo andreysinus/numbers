00187d43ad25b01472c97431e5616b6b7822b222
andreysinus <79992614510@ya.ru>
2022-06-27 14:19:03 +0300

    Added viewing of the dollar exchange rate

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-import os
 import psycopg2
-
+import time
 
 from bdConfig import host,user, password,db_name, gSheet_id
-from serviceFunc import get_service_sacc, createTable
-
+from serviceFunc import get_dollar, get_service_sacc, createTable
+usdCurrency=0
+x=True
 def main():
-    #try: 
+    try: 
         service = get_service_sacc()
         sheet = service.spreadsheets()
-        res = sheet.values().get(spreadsheetId=gSheet_id, range="Лист1!A1:D999").execute()
-        print(res)
         
+
         connection=psycopg2.connect(
                 user=user,
                 host=host,
@@ -19,14 +18,16 @@ def main():
                 database=db_name
             )
         createTable(connection)
-       
-    #except:
-        #print("Error while connection with PostgreSQL")
-    #finally:
-        #if connection:
-            #connection.close()
-            #print("PostgreSQL connection closed")
-
+        while x==True:
+            
+            time.sleep(60)
+            
+    except:
+        print("Error while connection with PostgreSQL")
+    finally:
+        if connection:
+            connection.close()
+            print("PostgreSQL connection closed")
 
 if __name__ == '__main__':
    main()
